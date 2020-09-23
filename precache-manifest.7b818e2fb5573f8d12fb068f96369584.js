e101f88a46315439f2b96dcaef12659f3c995f70
Jorge Arteaga <jorgelarteagap@gmail.com>
2020-09-23 13:13:59 -0400

    Updates

---
+++ precache-manifest.7b818e2fb5573f8d12fb068f96369584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f0276e5e1569ebcb14298273164b53",
+    "url": "/logintest/index.html"
+  },
+  {
+    "revision": "b08dce8f3471dc1672a6",
+    "url": "/logintest/static/css/main.1f714916.chunk.css"
+  },
+  {
+    "revision": "1983afad7ed3d08f9f3e",
+    "url": "/logintest/static/js/2.8bbbe002.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/logintest/static/js/2.8bbbe002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08dce8f3471dc1672a6",
+    "url": "/logintest/static/js/main.9844826a.chunk.js"
+  },
+  {
+    "revision": "da98df3f0b620d97a657",
+    "url": "/logintest/static/js/runtime-main.bf989d90.js"
+  }
+]);
